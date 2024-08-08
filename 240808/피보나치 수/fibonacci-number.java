f3772c1ab423d83199f67c33a7c9874270374cd4
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-08-08 23:38:33 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240808/피보나치 수/fibonacci-number.java
+++ 240808/피보나치 수/fibonacci-number.java
@@ -4,15 +4,15 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
+        int N = kb.nextInt();
 
-        System.out.println(fibbo(n));
-    }
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 1;
 
-    static int fibbo(int n) {
-        if (n <= 2) return 1;
-        else {
-            return fibbo(n - 1) + fibbo(n - 2);
+        for (int i = 3; i < N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
+        System.out.println(dp[N]);
     }
 }
