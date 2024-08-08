4e09b26519116e5eb404228a6bdea28dee6808af
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-08-08 23:41:57 +0900

    Title: 피보나치 수, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240808/피보나치 수/fibonacci-number.java
+++ 240808/피보나치 수/fibonacci-number.java
@@ -15,7 +15,7 @@ public class Main {
         dp[1] = 1;
         dp[2] = 1;
 
-        for (int i = 3; i < N; i++) {
+        for (int i = 3; i <= N; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         System.out.println(dp[N]);
