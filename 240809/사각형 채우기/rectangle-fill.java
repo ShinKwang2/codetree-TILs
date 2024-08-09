ed4a3f545d1a8c077a277a45a9f230b52766aa9b
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-08-09 10:47:18 +0900

    Title: 사각형 채우기, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 240809/사각형 채우기/rectangle-fill.java
+++ 240809/사각형 채우기/rectangle-fill.java
@@ -2,19 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    static int[] memo;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner kb = new Scanner(System.in);
         int N = kb.nextInt();
 
-        int[] dp = new int[N + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        memo = new int[N + 1];
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+        System.out.println(getCounts(N));
+    }
+    static int getCounts(int n) {
+        if (memo[n] != 0) {
+            return memo[n];
         }
 
-        System.out.println(dp[N]);
+        if (n <= 1) {
+            memo[n] = 1;
+        }
+        else {
+            memo[n] = (getCounts(n - 1) + getCounts(n - 2)) % 10007;
+        }
+
+        return memo[n];
     }
 }
