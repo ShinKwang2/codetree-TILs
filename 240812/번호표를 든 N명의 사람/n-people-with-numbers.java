72ca7c18c590b1a60bba36ba2afb75d67d1bcbed
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-08-12 22:49:34 +0900

    Title: 번호표를 든 N명의 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240812/번호표를 든 N명의 사람/n-people-with-numbers.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int MAX_TIME;
+    static int[] people;
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        N = kb.nextInt();
+        MAX_TIME = kb.nextInt();
+
+        people = new int[N];
+        for (int i = 0; i < N; i++) {
+            people[i] = kb.nextInt();
+        }
+
+        int left = 1;
+        int right = MAX_TIME;
+        int ans = Integer.MAX_VALUE;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isPossible(mid)) {
+                right = mid - 1;
+                ans = Math.min(ans, mid);
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static boolean isPossible(int size) {
+        Queue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < size; i++) {
+            pq.add(people[i]);
+        }
+        for (int i = size; i < N; i++) {
+            int min = pq.poll();
+            min += people[i];
+            pq.add(min);
+        }
+        int max = 0;
+        while (!pq.isEmpty()) {
+            max = Math.max(max, pq.poll());
+        }
+        return max <= MAX_TIME;
+    }
+}
