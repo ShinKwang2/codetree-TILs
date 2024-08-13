74103012050d2fb7a19574958491d163b9f68a06
ShinKwang2 <71647602+ShinKwang2@users.noreply.github.com>
2024-08-13 18:02:01 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 311ms, Memory: 20MB, Status: Passed - CodeTree

--- 240813/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 240813/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -6,11 +6,9 @@ public class Main {
     static int N;
     static int M;
     static int[][] map;
-    static boolean[][] visited;
+    static int[][] visited;
     static int[] dr = {1, 0};
     static int[] dc = {0, 1};
-
-    static boolean flag = false;
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
@@ -24,30 +22,23 @@ public class Main {
             }
         }
 
-        visited = new boolean[N][M];
-        visited[0][0] = true;
+        visited = new int[N][M];
+        visited[0][0] = 1;
         dfs(0, 0);
-        System.out.println(flag ? 1 : 0);
+        System.out.println(visited[N - 1][M - 1]);
     }
 
     public static void dfs(int r, int c) {
-        // System.out.println("r: " + r + "c: " + c);
 
         for (int d = 0; d < 2; d++) {
             int nextRow = r + dr[d];
             int nextCol = c + dc[d];
 
             if (nextRow < 0 || N <= nextRow || nextCol < 0 || M <= nextCol) continue; // 경계처리
-            if (visited[nextRow][nextCol]) continue; // 이미 방문
+            if (visited[nextRow][nextCol] == 1) continue; // 이미 방문
             if (map[nextRow][nextCol] == 0) continue; // 뱀자리
 
-            // System.out.println("nextRow: " + nextRow + " nextCol: " + nextCol);
-            // System.out.println("N: " + N + " M: " + M);
-
-            if (nextRow == N - 1 && nextCol == M - 1) {
-                flag = true;
-            }
-            visited[nextRow][nextCol] = true;
+            visited[nextRow][nextCol] = 1;
             dfs(nextRow, nextCol);
         }
     }
